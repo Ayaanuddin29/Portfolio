7bb8567c36ad04daaa38ce25c270df0af77c4504
Ayaanuddin29 <khajaayaan2912@gmil.com>
2024-09-07 19:41:55 +0530

    updated

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -12,12 +12,19 @@ const Ball = (props) => {
       <mesh castShadow receiveShadow scale={2.75}>
           <icosahedronGeometry args={[1,1]}/>
           <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading/>
-      <Decal map={decal} position={[0,0,1]} rotation={[2*Math.PI,0,6.25]} flatShading ap />
+          <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
+          map={decal}
+          flatShading
+        />
       </mesh>
     </Float>
   )
 }
-const BallCanvas=({icon})=>{
+const 
+BallCanvas=({icon})=>{
   return(
     <Canvas 
     frameloop='demand'
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,12 +5,12 @@ import CanvasLoader from '../Loader'
 const Earth = () => {
   const earth=useGLTF('./planet/scene.gltf')
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} />
-  )
-}
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0}/>
+  );
+};
 const EarthCanvas=()=>{
   return(
-    <Canvas shadows frameloop="demand" gl={{preserveDrawingBuffer:true}} camera={{fov:45,near:0.1,far:100,position:[-4,3,6]}}>
+    <Canvas shadows frameloop="demand" dpr={[1,2]} gl={{preserveDrawingBuffer:true}} camera={{fov:45,near:0.1,far:100,position:[-4,3,6]}}>
  <Suspense fallback={<CanvasLoader/>}>
  <OrbitControls
  autoRotate
@@ -19,6 +19,7 @@ const EarthCanvas=()=>{
  minPolarAngle={Math.PI/2}
  />
  <Earth/>
+ <Preload all/>
  </Suspense>
     </Canvas>
   )
