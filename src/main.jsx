63293d175e95daae31f229f76fc5f5d7ca27d52b
Ayaanuddin29 <khajaayaan2912@gmil.com>
2024-09-07 23:08:49 +0530

    fd

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'; // Correct import
-import App from './App.jsx';
+import App from './App';
 import './index.css';
 
 document.addEventListener('DOMContentLoaded', () => {
