ce2b3b84dd43e190a5245db81583c3bd920e541d
Ayaanuddin29 <khajaayaan2912@gmil.com>
2024-09-08 10:15:46 +0530

    g

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,7 +7,7 @@ const Computers = ({isMobile}) => {
   return (
     <mesh>
        <bufferGeometry attach="geometry" />
-    <hemisphereLight intensity={0.15} groundColor='black' />
+    <hemisphereLight intensity={4.15} groundColor='black' />
     <spotLight
       position={[-20, 50, 10]}
       angle={0.12}
@@ -16,7 +16,7 @@ const Computers = ({isMobile}) => {
       castShadow
       shadow-mapSize={1024}
     />
-    <pointLight intensity={1} />
+    <pointLight intensity={3} />
     <primitive
       object={computer.scene}
       scale={isMobile ? 0.7 : 0.75}
